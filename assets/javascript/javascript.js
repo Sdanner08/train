4a95412b40c622dd7c3401505c3c6b597f8d658d
Sdanner08 <37597715+Sdanner08@users.noreply.github.com>
2018-06-30 09:56:19 -0400

    Train

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,102 @@
+$(document).ready(function(){
+	/*FireBase
+	==============================================================*/
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDCW7Y61obJE-sEbsgQiZ7lS9P9eCj-vY8",
+    authDomain: "trains-ab434.firebaseapp.com",
+    databaseURL: "https://trains-ab434.firebaseio.com",
+    projectId: "trains-ab434",
+    storageBucket: "trains-ab434.appspot.com",
+    messagingSenderId: "381141234992"
+  };
+  firebase.initializeApp(config);
+	
+	  //Reference database
+	  database = firebase.database();
+	
+	/*Global Variables
+	==============================================================*/
+	var trainName = '';
+	var dest = '';
+	var firstTrainTime = ''; 
+	var freq = '';
+	
+	//Conversion Variable
+	var firstTimeConverted = '';
+	var diffTime = '';
+	var tRemainder;
+	var tMinutesTillTrain;
+	var nextTrain; 
+	
+	//Data reference
+	var trainNameData = '';
+	var destData = '';
+	var arrivalData = '';
+	var freqData = '';
+	var minutesAwayData = ''; 
+	 
+	/*Functions
+	==============================================================*/
+		//When Submit button is clicked.....
+		$('#submit').on('click',function(event){
+			event.preventDefault();
+			//Get input info
+			trainName = $('#trainName').val().trim();
+			dest = $('#dest').val().trim();
+			firstTrainTime = $('#firstTrainTime').val().trim();
+			freq = $('#freq').val().trim();
+	
+			//Removed input info 
+			$('#trainName').val('');
+			$('#dest').val('');
+			$('#firstTrainTime').val('');
+			$('#freq').val('');
+	
+			//Conversion
+				//Convert to HH:MM
+				firstTimeConverted = moment(firstTrainTime, "hh:mm").subtract(1, "years");
+				//Converts the firsTimeCover object into string
+				
+				 // Current Time
+				var currentTime = moment();
+				diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+				
+				// Time apart (remainder)
+				tRemainder = diffTime % freq;
+	
+				// Minute Until Train
+				tMinutesTillTrain = freq - tRemainder;
+				
+				// Next Train
+				nextTrain = moment().add(tMinutesTillTrain, "minutes");
+				nextTrainFormat = moment(nextTrain).format('hh:mm');
+			
+			database.ref('/trainSchedule').push({
+				trainName: trainName,
+				destination: dest,
+				arrival: nextTrainFormat,
+				minutesAway: tMinutesTillTrain,
+				frequency: freq 
+			}); 	
+		}); 
+			
+			database.ref('/trainSchedule').on('child_added',function(snap){
+						//Testing
+						trainNameData = snap.val().trainName;
+						destData = snap.val().destination;
+						arrivalData = snap.val().arrival;
+						freqData = snap.val().frequency;
+						minutesAwayData = snap.val().minutesAway;
+	
+						//Data array
+						var dataArray = [trainNameData, destData, freqData, arrivalData, minutesAwayData];
+						var newTr = $('<tr>');
+						for(var i = 0; i< dataArray.length; i++){
+							var newTd = $('<td>');
+							newTd.text(dataArray[i]);
+							newTd.appendTo(newTr);
+						}	
+						$('.table').append(newTr);
+			});
+	});
